0f94a20b56cd07dee50559ac3b9c2f99d6d7c441
Richard Gilbert <gilbert67@gmail.com>
2021-09-28 10:15:24 -0700

    Updates to shows consumer; simplifying custom fields.

--- wp-content/plugins/gicinema-get-shows/shows-consumer.php
+++ wp-content/plugins/gicinema-get-shows/shows-consumer.php
@@ -1,30 +1,77 @@
-<?php /* Template Name: CustomPageT1 */ ?>
+<?php /* Shows consumer. */ ?>
 
-<?php get_header(); ?>
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'].'/wp-load.php');
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-			<?php
-			// Start the loop.
-			while ( have_posts() ) : the_post();
+function shows_consumer() {
 
-				// Include the page content template.
-				get_template_part( 'template-parts/content', 'page' );
+	$url = 'https://prod5.agileticketing.net/websales/feed.ashx?guid=52c1280f-be14-4579-8ddf-4b3dadbf96c7&showslist=true&withmedia=true&format=json&v=latest';
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) {
-					comments_template();
-				}
+	$args = array(
+		'method' => 'GET'
+	);
+	$response = wp_remote_get( $url, $args );
+	if ( is_wp_error( $response ) ) {
+		$error_msg = $response->get_error_message();
+		return "Something went wrong: $error_message";
+	}
 
-				// End of the loop.
-			endwhile;
-			?>
+	$results = json_decode( wp_remote_retrieve_body( $response ) );
 
-		</main><!-- .site-main -->
+	foreach( $results->ArrayOfShows as $show ) {
+		echo '<div>';
+		echo '<b>ID:</b> ' . $show->ID . '<br>';
+		echo '<b>Name:</b> ' . $show->Name . '<br>';
 
-		<?php get_sidebar( 'content-bottom' ); ?>
+        $matchingFilms = get_posts([
+            'post_type'  => 'film',
+            'title' => $show->Name,
+        ]);
 
-	</div><!-- .content-area -->
+        if ( empty($matchingFilms) ) {
+            echo '<b><i>Creating new film...</i></b><br>';
+	        // Create post object
+	        $newMovie = array(
+	            'post_type'     => 'film',
+		        'post_title'    => wp_strip_all_tags( $show->Name ),
+		        'post_status'   => 'publish'
+	        );
+	        // Insert the post into the database
+	        $newMovieID = wp_insert_post($newMovie);
+	        add_post_meta($newMovieID, 'description', $show->ShortDescription, true);
+	        add_post_meta($newMovieID, 'film_length', $show->Duration, true);
 
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+	        foreach( $show->CustomProperties as $customProp ) {
+
+	            echo $customProp->Name;
+
+		        if ( $customProp->Name == 'Release Year' ) {
+                    echo '<b>Year:</b> ' . $customProp->Value . '<br>';
+                    add_post_meta($newMovieID, 'film_year', $customProp->Value, true);
+		        }
+
+		        if ( $customProp->Name == 'Format' ) {
+			        echo '<b>Format:</b> ' . $customProp->Value . '<br>';
+			        add_post_meta($newMovieID, 'format', $customProp->Value, true);
+		        }
+
+		        if ( $customProp->Name == 'Director' ) {
+			        echo '<b>Director:</b> ' . $customProp->Value . '<br>';
+			        add_post_meta($newMovieID, 'film_director', $customProp->Value, true);
+		        }
+	        }
+
+        }
+        else {
+	        echo 'UPDATE (maybe)!<br>';
+	        foreach ( $matchingFilms as $matchingFilm ) {
+	            $thisMatchingFilm = get_post( $matchingFilm );
+	            print_r($thisMatchingFilm);
+	        }
+        }
+
+		echo '</div><hr />';
+	}
+}
+
+shows_consumer();
