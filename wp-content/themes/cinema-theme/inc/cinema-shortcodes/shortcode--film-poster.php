0023f5ffd49199289cfec7593a52d9f0673e698e
Richard Gilbert <gilbert67@gmail.com>
2022-10-22 20:38:06 -0700

    Displaying local poster.

--- wp-content/themes/cinema-theme/inc/cinema-shortcodes/shortcode--film-poster.php
+++ wp-content/themes/cinema-theme/inc/cinema-shortcodes/shortcode--film-poster.php
@@ -4,11 +4,15 @@ function film_poster_function($atts = [], $tag = '') {
 
   $output = '';
   $post_id = get_the_ID();
-  $poster = get_field('poster_url', $post_id);
 
+  /*
+  $poster = get_field('poster_url', $post_id);
   if( $poster ) {
     $output = '<div class="film-poster"><img src="' . $poster . '"></div>';
   }
+  */
+
+  $output = get_the_post_thumbnail( $post_id );
 
   return $output;
 }
