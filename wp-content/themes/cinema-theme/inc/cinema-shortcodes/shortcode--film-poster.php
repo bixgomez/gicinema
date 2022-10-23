04c56d17d6bee783f6b3ff9b83788e600e3c30f8
Richard Gilbert <gilbert67@gmail.com>
2022-10-22 20:44:41 -0700

    Poster image conditional.

--- wp-content/themes/cinema-theme/inc/cinema-shortcodes/shortcode--film-poster.php
+++ wp-content/themes/cinema-theme/inc/cinema-shortcodes/shortcode--film-poster.php
@@ -2,19 +2,19 @@
 
 function film_poster_function($atts = [], $tag = '') {
 
-  $output = '';
-  $post_id = get_the_ID();
+    $output = '';
+    $post_id = get_the_ID();
 
-  /*
-  $poster = get_field('poster_url', $post_id);
-  if( $poster ) {
-    $output = '<div class="film-poster"><img src="' . $poster . '"></div>';
-  }
-  */
+    if (has_post_thumbnail($post_id)) {
+        $output = get_the_post_thumbnail($post_id);
+    } else {
+        $poster = get_field('poster_url', $post_id);
+        if ($poster) {
+            $output = '<div class="film-poster"><img src="' . $poster . '"></div>';
+        }
+    }
 
-  $output = get_the_post_thumbnail( $post_id );
-
-  return $output;
+    return $output;
 }
 
 add_shortcode('film_poster', 'film_poster_function');
