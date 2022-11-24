83160afd1e15a3358c0ee6acf06f64cdc89f30b3
Richard Gilbert <gilbert67@gmail.com>
2022-11-23 21:13:28 -0800

    Updating cron job (commenting out old code)

--- wp-content/plugins/gicinema-shows-importer/shows-importer--cron.php
+++ wp-content/plugins/gicinema-shows-importer/shows-importer--cron.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * I am pretty sure we aren't using this script.
+ */
+
+/*
 require_once "shows-importer--function.php";
 
 add_action( 'shows_importer_hook', 'shows_importer' );
@@ -7,3 +12,4 @@ add_action( 'shows_importer_hook', 'shows_importer' );
 if ( ! wp_next_scheduled( 'shows_importer_hook' ) ) {
     wp_schedule_event( time(), 'five_seconds', 'shows_importer_hook' );
 }
+*/
