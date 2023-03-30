d4dc6316746c1949c895712c05d4557a34459023
Richard Gilbert <gilbert67@gmail.com>
2023-03-30 11:46:08 -0700

    Working on new home page.

--- wp-content/themes/cinema-theme/page--home__new.php
+++ wp-content/themes/cinema-theme/page--home__new.php
@@ -105,6 +105,58 @@ get_header();
 
 <h2>Coming Soon</h2>
     <li>Get all the movies that have upcoming screenings that are NOT in the first set</li>
+
+    <li>
+        First, get 100 dates starting with today
+
+        <?php 
+        $days = [];
+        $period = new DatePeriod(
+            new DateTime(date_i18n("Y-m-d 00:00:00")), // Start date of the period
+            new DateInterval('P1D'), // Define the intervals as Periods of 1 Day
+            100 // Apply the interval 100 times on top of the starting date
+        );
+        foreach ($period as $day) {
+            $days[] = '\''.$day->format('Y-m-d').'\'';
+        }
+        $daysString = implode (',', $days);
+        print '<pre>';
+        print_r($days);
+        print_r($daysString);
+        print '</pre>';
+        ?>
+    </li>
+
+    <li>
+        Next, get all the movies that have screenings on those dates
+
+        <?php
+        global $wpdb;
+        $screenings_table_name = $wpdb->prefix . 'gi_screenings';
+        $screenings_query = "
+            SELECT film_id, screening
+            FROM {$screenings_table_name} 
+            WHERE screening_date IN ($daysString)
+            ORDER BY screening
+        ";
+        echo '<pre>' . $screenings_query . '</pre>';
+        $result = $wpdb->get_results($screenings_query);
+        if ( count($result) ) :
+            $nowPlayingFilmIds = [];
+            foreach( $result as $key => $row) :
+                echo $row->film_id . ': ' . $row->screening . '<br>';
+                if (!in_array($row->film_id, $nowPlayingFilmIds)) :
+                    $nowPlayingFilmIds[] = $row->film_id;
+                endif;
+            endforeach;
+            print '<pre>';
+            print_r($nowPlayingFilmIds);
+            print '</pre>';
+        endif;
+        ?>
+
+    </li>
+
     <li>Display their "half teasers", in "next screening" order</li>
 <hr />
 
