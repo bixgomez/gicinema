c0948987c78cc874f35e76ad36b8c65ab8f4e6a7
Richard Gilbert <gilbert67@gmail.com>
2023-09-15 22:41:24 -0700

    Plugin updates

--- wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit0198cf30433eb0da4c0d76a0071fb247::getLoader();
+return ComposerAutoloaderInitc57d044a24bfb26a5a62055272330da7::getLoader();
