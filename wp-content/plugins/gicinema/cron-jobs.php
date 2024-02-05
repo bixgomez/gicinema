616543c87966f1f3a0adfbfc6b4133775d624490
Richard Gilbert <gilbert67@gmail.com>
2024-02-05 10:50:44 -0800

    Updating cron job.

--- wp-content/plugins/gicinema/cron-jobs.php
+++ wp-content/plugins/gicinema/cron-jobs.php
@@ -1,15 +1,15 @@
 <?php 
 
-require_once "function__import_from_agile.php";
+require_once "function__import_films_from_agile.php";
 
-add_action('import_from_agile_hook', 'import_from_agile');
-if (!wp_next_scheduled('import_from_agile_hook')) {
-    wp_schedule_event(time(), 'half_hourly', 'import_from_agile_hook');
+add_action('import_films_from_agile_hook', 'import_films_from_agile');
+if (!wp_next_scheduled('import_films_from_agile_hook')) {
+    wp_schedule_event(time(), 'half_hourly', 'import_films_from_agile_hook');
 }
 
 // Custom interval
-add_filter('cron_schedules', 'import_from_agile_hook_interval');
-function import_from_agile_hook_interval($schedules) {
+add_filter('cron_schedules', 'import_films_from_agile_hook_interval');
+function import_films_from_agile_hook_interval($schedules) {
     $schedules['half_hourly'] = array(
         'interval' => 1800,
         'display' => esc_html__('Every Half Hour'),
