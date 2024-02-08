4367d9057abdc7f934c87f3877eb8d7ed7e95b69
Richard Gilbert <gilbert67@gmail.com>
2024-02-07 23:39:09 -0800

    updating screenings on film card and calendar colors.

--- wp-content/themes/cinema-theme/inc/functions/film-card.php
+++ wp-content/themes/cinema-theme/inc/functions/film-card.php
@@ -121,41 +121,48 @@ function filmCard($filmPostId, $classes='film') {
         </div>
 
         <?php if (isset($screenings) && is_array($screenings) && count($screenings) > 0) : ?>
-        <div class="film-card--screenings">
-          <div class="screenings">
-            <ul class="screenings-list">
-              <?php 
-              $screeningsArray = array_map(function ($item) {
-                return $item['screening'];
-              }, $screenings);
+          <div class="film-card--screenings">
+            <div class="screenings">
+              <ul class="screenings-list">
+                <?php 
+                $screeningsArray = array_map(function ($item) {
+                  return $item['screening'];
+                }, $screenings);
 
-              // Sort the screenings array in ascending order
-              usort($screeningsArray, "date_compare");
+                // Sort the screenings array in ascending order
+                usort($screeningsArray, "date_compare");
 
-              $timezone = new DateTimeZone('America/Los_Angeles');
-              $today = (new DateTime('now', $timezone))->setTime(0, 0, 0);
-              $twoMonthsAgo = (new DateTime('-2 months', $timezone))->setTime(0, 0, 0); 
+                $timezone = new DateTimeZone('America/Los_Angeles');
+                $today = (new DateTime('now', $timezone))->setTime(0, 0, 0);
+                $twoMonthsAgo = (new DateTime('-2 months', $timezone))->setTime(0, 0, 0);
 
-              foreach ($screeningsArray as $screening_date) {
-                $dateTime = new DateTime($screening_date);
-                $dateTimeCompare = (new DateTime($screening_date, $timezone))->setTime(0, 0, 0);
-                if ($dateTimeCompare > $twoMonthsAgo) {
-                  $formattedDate = $dateTime->format('l, M j');
-                  $formattedTime = $dateTime->format('g:ia');
-                  if ($dateTimeCompare < $today):
-                    $extraClass = 'past';
-                  elseif ($dateTimeCompare == $today):
-                    $extraClass = 'present';
-                  elseif ($dateTimeCompare > $today):
-                    $extraClass = 'future';
-                  endif;
-                  echo '<li class="screening '.$extraClass.'">' . $formattedDate . ', ' . $formattedTime . '</li>';
+                // date/time object of the most recent screening of this film.
+                $lastScreeningCompare = (new DateTime(end($screeningsArray), $timezone))->setTime(0, 0, 0);
+
+                foreach ($screeningsArray as $screening_date) {
+                  $dateTime = new DateTime($screening_date);
+
+                  // If the screening is current, only display screening from the most recent 2 months.
+                  // If it's an older screening, show all of the screenings.
+                  $dateTimeCompare = (new DateTime($screening_date, $timezone))->setTime(0, 0, 0);
+                  if (  ($lastScreeningCompare < $twoMonthsAgo) || ($dateTimeCompare > $twoMonthsAgo) ) {
+                    $formattedDate = $dateTime->format('l, M j, Y');
+                    $formattedTime = $dateTime->format('g:ia');
+                    if ($dateTimeCompare < $today):
+                      $extraClass = 'past';
+                    elseif ($dateTimeCompare == $today):
+                      $extraClass = 'present';
+                    elseif ($dateTimeCompare > $today):
+                      $extraClass = 'future';
+                    endif;
+                    echo '<li class="screening '.$extraClass.'">' . $formattedDate . ', ' . $formattedTime . '</li>';
+                  }
+
                 }
-              }
-              ?>
-            </ul>
+                ?>
+              </ul>
+            </div>
           </div>
-        </div>
         <?php endif ?>
 
         <div class="film-card--description">
