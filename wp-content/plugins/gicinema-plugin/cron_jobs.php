7f141bb5130a31944b7d50095bc183aef1a107e4
Richard Gilbert <gilbert67@gmail.com>
2024-02-20 01:04:31 -0800

    Adding cron job to update and sync screenings data.

--- wp-content/plugins/gicinema-plugin/cron_jobs.php
+++ wp-content/plugins/gicinema-plugin/cron_jobs.php
@@ -3,35 +3,48 @@
 // If this file is called directly, abort!
 defined('ABSPATH') or die('Unauthorized Access');
 
+// Loading functions we'll be running as cron jobs.
 require_once "function__import_films_from_agile.php";
+require_once "function__manage_screenings.php";
 
+// Setting up custom cron intervals.
 function gicinema__hook_interval($schedules) {
-    $schedules['half_hourly'] = array(
+    $schedules['every_20_minutes'] = array(
+        'interval' => 1200,
+        'display' => esc_html__('Every 20 Minutes'),
+    );
+    $schedules['every_30_minutes'] = array(
         'interval' => 1800,
-        'display' => esc_html__('Every Half Hour'),
+        'display' => esc_html__('Every 30 Minutes'),
+    );
+    $schedules['every_50_minutes'] = array(
+        'interval' => 3000,
+        'display' => esc_html__('Every 50 Minutes'),
     );
     return $schedules;
 }
-
 add_filter('cron_schedules', 'gicinema__hook_interval');
 
-if (!wp_next_scheduled('gicinema__agile_importer')) {
-    wp_schedule_event(time(), 'half_hourly', 'gicinema__agile_importer');
+// Cron job to manage screenings.
+if (!wp_next_scheduled('cron__manage_screenings')) {
+    wp_schedule_event(time(), 'every_50_minutes', 'cron__manage_screenings');
 }
+add_action('cron__manage_screenings', 'gicinema__manage_screenings');
 
-add_action('gicinema__agile_importer', 'gicinema__import_films_from_agile');
-
-
-
-
-
-if ( ! wp_next_scheduled( 'gicinema__update_agile_shows_array' ) ) {
-    wp_schedule_event( time(), 'half_hourly', 'gicinema__update_agile_shows_array' );
+// Cron job to import films from Agile.
+if (!wp_next_scheduled('cron__import_films_from_agile')) {
+    wp_schedule_event(time(), 'every_30_minutes', 'cron__import_films_from_agile');
 }
+add_action('cron__import_films_from_agile', 'gicinema__import_films_from_agile');
 
-add_action( 'gicinema__update_agile_shows_array', 'fetch_and_store_api_response' );
+// Cron job to run the Agile shows fetcher.
+if ( ! wp_next_scheduled( 'cron__update_agile_shows_array' ) ) {
+    wp_schedule_event( time(), 'every_20_minutes', 'cron__update_agile_shows_array' );
+}
+add_action( 'cron__update_agile_shows_array', 'gicinema__update_agile_shows_array' );
 
-function fetch_and_store_api_response() {
+// Function that fetches and stores the Agile data as a transient.
+function gicinema__update_agile_shows_array() {
     $url = 'https://prod5.agileticketing.net/websales/feed.ashx?guid=52c1280f-be14-4579-8ddf-4b3dadbf96c7&showslist=true&withmedia=true&format=json&v=latest';
     $args = array( 'method' => 'GET' );
     $response = wp_remote_get( $url, $args );
