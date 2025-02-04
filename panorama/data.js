7ad593b7a6514cecd938ec9093fba631496841ff
Richard Gilbert <gilbert67@gmail.com>
2025-02-04 00:23:39 -0800

    Adding new booth photo.

--- panorama/data.js
+++ panorama/data.js
@@ -72,6 +72,12 @@ var APP_DATA = {
           "pitch": 0.5484843608324219,
           "rotation": 0,
           "target": "2-r0011099"
+        },
+        {
+          "yaw": -3.083207544821244,
+          "pitch": 0.6961313890835417,
+          "rotation": 0,
+          "target": "3-r0011106"
         }
       ],
       "infoHotspots": []
@@ -109,6 +115,44 @@ var APP_DATA = {
           "yaw": 0.16562361401239833,
           "pitch": 0.5150649472766649,
           "rotation": 0,
+          "target": "1-r0011105"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-r0011106",
+      "name": "R0011106",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": -0.004376709320681016,
+        "pitch": 0.41353603676563466,
+        "fov": 1.8086621154208748
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0268466015764659,
+          "pitch": 0.9324493775535672,
+          "rotation": 7.853981633974483,
           "target": "1-r0011105"
         }
       ],
